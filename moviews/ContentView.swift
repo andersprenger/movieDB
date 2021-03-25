2b314e8ae1bb9d6d85dde5038ba8a273b79848b8
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2021-03-25 14:52:57 -0300

    Initial Commit

---
+++ moviews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  moviews
+//
+//  Created by Anderson Sprenger on 25/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ moviews/moviewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  moviewsApp.swift
+//  moviews
+//
+//  Created by Anderson Sprenger on 25/03/21.
+//
+
+import SwiftUI
+
+@main
+struct moviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
